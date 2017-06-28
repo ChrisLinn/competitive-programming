c6f8233b1daac4b2b44dc045964055a070c6d617
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-06-29 04:02:19 +1000

    sorted

--- LeetCode/501-Find_Mode_in_Binary_Search_Tree/501.1.cpp
+++ LeetCode/501-Find_Mode_in_Binary_Search_Tree/501.1.cpp
@@ -9,7 +9,21 @@
 */
 class Solution {
 public:
-vector<int> modes;
-vector<int> findMode(TreeNode* root) {
+    vector<int> modes;
+    
+    vector<int> nodes;
+    
+    vector<int> findMode(TreeNode* root) {
+        traverse(root);
+        sort(nodes.begin(), nodes.end());
+        return nodes;
+    }
+    
+    void traverse(TreeNode* root) {
+        if (root) {
+            traverse(root->left);
+            nodes.push_back(root->val);
+            traverse(root->right);
+        }
     }
 };
