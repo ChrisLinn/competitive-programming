d50732c620487854569c73873f0983a688dcc1c6
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-06-29 09:13:44 +0800

    update

--- LeetCode/22-Generate_Parentheses/22.cpp
+++ LeetCode/22-Generate_Parentheses/22.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> result;
         vector<string> current;
         current.push_back("");
-        dfs(current,0,0,n);
+        return dfs(current,0,0,n);
     }
 };
 
@@ -13,33 +12,45 @@ vector<string> dfs(vector<string> current,int x,int y,int n){
 
     vector<string> down_result = down(current,x,y);
     if (down_result.size()!=0) {
-        level_result.
+        level_result.insert(level_result.end(), 
+                            down_result.begin(), down_result.end());
     }
 
-    vector<string> down_result = right(current,x,y);
-    if (down_result.size()!=0) {
-        level_result.
+    vector<string> right_result = right(current,x,y);
+    if (right_result.size()!=0) {
+        level_result.insert(level_result.end(), 
+                            right_result.begin(), right_result.end());
     }
 
     return level_result;
 }
 
 vector<string> down(vector<string> current,int x,int y,int n){
-    for string in current {
-        string +'(';
-    }
+    // for string in current {
+    //     string +'(';
+    // }
     x++;
-    if (x>n||Y>n||(x+y)>2*n) {return current.clear();}
-    else if ((x+y)==2*n) return current;
-    else return dfs(current,x,y,n);
+    if (x>n||Y>n||(x+y)>2*n) {
+        return current.clear();
+    } else if ((x+y)==2*n) {
+        return current;
+    }
+    else {
+        return dfs(current,x,y,n);
+    }
 }
 
 vector<string> right(vector<string> current,int x,int y,int n){
-    for string in current {
-        string +')';
-    }
+    // for string in current {
+    //     string +')';
+    // }
     y++;
-    if (x>n||Y>n||(x+y)>2*n) {return current.clear();}
-    else if ((x+y)==2*n) return current;
-    else return dfs(current,x,y,n);
+    if (x>n||Y>n||(x+y)>2*n) {
+        return current.clear();
+    } else if ((x+y)==2*n) {
+        return current;
+    }
+    else {
+        return dfs(current,x,y,n);
+    }
 }
