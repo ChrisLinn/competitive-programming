bc7c5364447c7ebe366524f5308eaaae633bba29
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-06-29 08:49:34 +0800

    init 22

--- LeetCode/22-Generate_Parentheses/22.cpp
+++ LeetCode/22-Generate_Parentheses/22.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        
+        vector<string> result;
+        vector<string> current;
+        current.push_back("");
+        dfs(current,0,0);
     }
 };
