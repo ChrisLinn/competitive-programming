d06e9bdbfb110ba257dc8da694db291ba6263964
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-06-29 09:02:02 +0800

    setch

--- LeetCode/22-Generate_Parentheses/22.cpp
+++ LeetCode/22-Generate_Parentheses/22.cpp
@@ -7,3 +7,39 @@ public:
         dfs(current,0,0);
     }
 };
+
+vector<string> dfs(current,x,y){
+    vector<string> level_result;
+
+    vector<string> down_result = down(current,x,y);
+    if (down_result.size()!=0) {
+        level_result.
+    }
+
+    vector<string> down_result = right(current,x,y);
+    if (down_result.size()!=0) {
+        level_result.
+    }
+
+    return level_result;
+}
+
+vector<string> down(current,x,y){
+    for string in current {
+        string +'(';
+    }
+    x++;
+    if (x>n||Y>n||(x+y)>2*n) {return current.clear()}
+    else if ((x+y)==2*n) return current;
+    else return dfs(current,x,y)
+}
+
+vector<string> right(current,x,y){
+    for string in current {
+        string +')';
+    }
+    y++;
+    if (x>n||Y>n||(x+y)>2*n) {return current.clear()}
+    else if ((x+y)==2*n) return current;
+    else return dfs(current,x,y);
+}
