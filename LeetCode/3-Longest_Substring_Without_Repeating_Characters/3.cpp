f9f7c4a7747e2791b57c882e779a4ac580a12c5a
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-07 15:48:26 +1100

    update lengthOfLongestSubstring

--- LeetCode/3-Longest_Substring_Without_Repeating_Characters/3.cpp
+++ LeetCode/3-Longest_Substring_Without_Repeating_Characters/3.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring1(string s) {
         int len = s.length();
         unordered_set<char> myset;
         int ans = 0, i = 0, j = 0;
@@ -17,6 +17,19 @@ public:
             }            
         }           
 
+        return ans;
+    }
+
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length(), ans = 0;
+        map<char,int> map;
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.find(s[j]) != map.end()) {
+                i = map[s[j]] > i ? map[s[j]] : i;
+            }
+            ans = ans > j-i+1 ? ans: j-i+1;
+            map[s[j]] = j + 1;
+        }        
         return ans;
     }
 };
