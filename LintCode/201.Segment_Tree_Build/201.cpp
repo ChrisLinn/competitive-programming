d129687372f1feae06c0426bd364604e3f4ec775
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-08 19:32:39 -0800

    li-201 感觉被改简单

--- LintCode/201.Segment_Tree_Build/201.cpp
+++ LintCode/201.Segment_Tree_Build/201.cpp
@@ -21,6 +21,9 @@ public:
      */
     SegmentTreeNode * build(int start, int end) {
         // write your code here
+        if(start>end)
+            return NULL;
+
         SegmentTreeNode* res = new SegmentTreeNode(start, end);
         
         if (start != end) {
@@ -31,3 +34,48 @@ public:
         return res;
     }
 };
+
+
+
+// https://github.com/zhuli19901106/lintcode/blob/master/segmemt-tree-build-ii(AC).cpp
+// 有 max
+// /**
+//  * Definition of SegmentTreeNode:
+//  * class SegmentTreeNode {
+//  * public:
+//  *     int start, end, max;
+//  *     SegmentTreeNode *left, *right;
+//  *     SegmentTreeNode(int start, int end, int max) {
+//  *         this->start = start;
+//  *         this->end = end;
+//  *         this->max = max;
+//  *         this->left = this->right = NULL;
+//  *     }
+//  * }
+//  */
+// class Solution {
+// public:
+//     /**
+//      *@param A: a list of integer
+//      *@return: The root of Segment Tree
+//      */
+//     SegmentTreeNode *build(vector<int> &A) {
+//         return !A.empty() ? buildRecursive(A, 0, A.size() - 1) : NULL;
+//     }
+// protected:
+//     SegmentTreeNode *buildRecursive(vector<int> &a, int start, int end) {
+//         SegmentTreeNode *root = NULL;
+//         if (start == end) {
+//             root = new SegmentTreeNode(start, end, a[start]);
+//         } else {
+//             int mid = start + (end - start) / 2;
+//             SegmentTreeNode *left = buildRecursive(a, start, mid);
+//             SegmentTreeNode *right = buildRecursive(a, mid + 1, end);
+//             root = new SegmentTreeNode(start, end, max(left->max, right->max));
+//             root->left = left;
+//             root->right = right;
+//         }
+        
+//         return root;
+//     }
+// };
