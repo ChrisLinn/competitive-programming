900cf658be0fd591b87db2786be83f5db074373e
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-09 11:05:27 +1100

    update le3

--- LeetCode/3-Longest_Substring_Without_Repeating_Characters/3.cpp
+++ LeetCode/3-Longest_Substring_Without_Repeating_Characters/3.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    int lengthOfLongestSubstring2(string s)  {
+        const int ASCII_MAX = 255;
+        int last[ASCII_MAX]; // 记录字符上次出现过的位置
+        int start = 0; // 记录当前子串的起始位置
+        fill(last, last + ASCII_MAX, -1); // 0也是有效位置,因此初始化为-1
+        int max_len = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (last[s[i]] >= start) {
+                max_len = max(i - start, max_len);
+                start = last[s[i]] + 1;
+            }
+            last[s[i]] = i;
+        }
+        return  max((int)s.size() - start, max_len); // 别忘了最后一次,例如"abcd"
+    }
+
+
+
     int lengthOfLongestSubstring1(string s) {
         int len = s.length();
         unordered_set<char> myset;
