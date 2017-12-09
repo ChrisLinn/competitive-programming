9710c5e9dbb03b01822e24e124555eec10fa8d90
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-08 20:15:21 -0800

    li-5 patial_sort, nth_element, lamda

--- LintCode/5.Kth_Largest_Element/5.cpp
+++ LintCode/5.Kth_Largest_Element/5.cpp
@@ -6,6 +6,58 @@ public:
      * @return: the Kth largest element
      */
     int kthLargestElement(int n, vector<int> &nums) {
+        // write your code here
+        nth_element (nums.begin(), nums.begin() + n, nums.end(), 
+                [](const int a, const int b) -> bool
+                { 
+                    return a > b; 
+                }
+            );
+        for (int a: nums) {
+            cout<<a;
+        }
+        return nums[n-1];
+    }
+    
+    int kthLargestElement4(int n, vector<int> &nums) {
+        // write your code here
+        nth_element (nums.begin(), nums.begin() + n, nums.end(), 
+                [](const int a, const int b) -> bool
+                { 
+                    return a > b; 
+                }
+            );
+        for (int a: nums) {
+            cout<<a;
+        }
+        return nums[n-1];
+    }
+
+    int kthLargestElement3(int n, vector<int> &nums) {
+        // write your code here
+        partial_sort (nums.begin(), nums.begin() + n, nums.end(), 
+                [](const int a, const int b) -> bool
+                { 
+                    return a > b; 
+                }
+            );
+        for (int a: nums) {
+            cout<<a;
+        }
+        return nums[n-1];
+    }
+
+    int kthLargestElement2(int n, vector<int> &nums) {
+        // write your code here        
+        // partial_sort (nums.begin(), nums.begin() + nums.size() - n + 1, nums.end());
+        partial_sort (nums.begin(), nums.end() - n + 1, nums.end());
+        for (int a: nums) {
+            cout<<a;
+        }
+        return nums[nums.size() - n];
+    }
+
+    int kthLargestElement1(int n, vector<int> &nums) {
         // write your code here        
         sort(nums.begin(), nums.end());
         return nums[nums.size() - n];
