5744acf7ecd07e93353b08185fd83027348e1adf
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-11 23:52:42 +1100

    li-116] greedy

--- LintCode/116.Jump_Game/116.cpp
+++ LintCode/116.Jump_Game/116.cpp
@@ -22,3 +22,44 @@ public:
         return res[A.size()-1];
     }
 };
+
+class Solution {
+public:
+    /**
+     * @param A: A list of integers
+     * @return: The boolean answer
+     */
+    bool canJump(vector<int> A) {
+        int reachable = 0;
+        for (int i = 0; i < A.size(); ++i) {
+            if (i > reachable) {
+                return false;
+            }
+            reachable = max(reachable, i + A[i]);
+        }
+
+        return true;
+    }
+};
+
+
+
+class Solution2 {
+public:
+    /**
+     * @param A: A list of integers
+     * @return: The boolean answer
+     */
+    bool canJump(vector<int> A) {
+        // write you code here
+        int sz = A.size();
+        
+        int reachable = A[0];
+        for(int i=0; i<=reachable; ++i){
+            reachable = max(reachable, A[i] + i);
+            if (reachable >= sz-1)
+                return true;
+        }
+        return false;
+    }
+};
