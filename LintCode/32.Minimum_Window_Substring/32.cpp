cc80cc8d8c70b83677f67c39029cc6d25bba8d9c
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-12 15:44:35 +1100

    li-32] ???

--- LintCode/32.Minimum_Window_Substring/32.cpp
+++ LintCode/32.Minimum_Window_Substring/32.cpp
@@ -7,5 +7,48 @@ public:
      */
     string minWindow(string &source , string &target) {
         // write your code here
+        int i, j, res_i, res_j;
+        int covered = 0;
+        std::set<char> alreadyCover;
+
+        for (i = 0, j = 0, res_i = 0, res_j = source.length(); i < source.length(); ) {
+
+            if (target.find(source[j]) != std::string::npos) { // source[j] in target
+                if (alreadyCover.find(source[j]) == alreadyCover.end()) { // !alreadyCover(source[j])
+                    ++covered;
+                    alreadyCover.emplace(source[j]);
+                } else { // alreadyCover(source[j])
+                    if (source[j] != source[i]) {
+                        i = j;
+                        covered = 0;
+                        alreadyCover.clear();
+                    } else {
+                        while (alreadyCover.find(source[++i]) == alreadyCover.end()) { // !alreadyCover(source[++i])
+                        }
+                    }
+                }
+            }
+
+            if (j < source.length()) {
+                ++j;
+            }
+
+            if (covered == target.length()) {
+                if ((j - i) < (res_j - res_i)) {
+                    res_i = i;
+                    res_j = j;
+                }
+            }
+
+            if (j == source.length()) {
+                ++i;
+            }
+        }
+
+        if (covered == target.length()) {
+            return source.substr(res_i, res_j);
+        } else {
+            return "";
+        }
     }
 };
