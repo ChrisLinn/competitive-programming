8355ad23a19560b58c015d34071d94739857bbd7
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-15 21:40:49 +1100

    li-174]cntLength

--- 174.Remove_Nth_Node_From_End_of_List/174.cpp
+++ 174.Remove_Nth_Node_From_End_of_List/174.cpp
@@ -11,6 +11,47 @@
  * }
  */
 
+
+class Solution1 {
+public:
+    /*
+     * @param head: The first node of linked list.
+     * @param n: An integer
+     * @return: The head of linked list.
+     */
+    ListNode * removeNthFromEnd(ListNode * head, int n) {
+        // write your code here
+        int length = cntLength(head, 0);
+        ListNode * cur = head;
+        for (int i = 0; i < (length - n - 1); ++i) {
+            cout<<(cur->val)<<endl;
+            cur = cur->next;
+        }
+        
+        if (head->next == nullptr) {
+            return NULL;
+        } else if (cur == head) {
+            return head->next;
+        } else {
+            cur->next = cur->next->next;
+            return head;
+        }
+    }
+
+private:
+    int cntLength(ListNode * head, int n) {
+        if (!head) {
+            return n;
+        } else {
+            return cntLength(head->next, n + 1);
+        }
+    }
+};
+
+
+
+
+
 // Time:  O(n)
 // Space: O(1)
 class Solution {
