e411f056f43c9887470b8527e36e9e814f7408d4
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-22 17:08:17 +0800

    li-445] ac

--- LintCode/445.Cosine_Similarity/445.cpp
+++ LintCode/445.Cosine_Similarity/445.cpp
@@ -7,5 +7,39 @@ public:
      */
     double cosineSimilarity(vector<int> &A, vector<int> &B) {
         // write your code here
+
+        double numerator, denominator1, denominator2, denominator;
+        numerator = 0;
+        denominator1 = 0;
+        denominator2 = 0;
+        denominator = 0;
+
+        if (A.size() != B.size()) {
+            return 2.0000;
+        }
+
+        for (std::vector<int>::iterator it_a = A.begin(), it_b = B.begin();
+            it_a != A.end();
+            ++it_a, ++it_b) {
+            numerator += (*it_a)*(*it_b);
+        }
+
+        for (auto a: A) {
+            denominator1 += a*a;
+        }
+        if (!denominator1) {
+            return 2.0000;
+        }
+
+        for (auto b: B) {
+            denominator2 += b*b;
+        }
+        if (!denominator2) {
+            return 2.0000;
+        }
+
+        denominator = pow(denominator1*denominator2, 0.5);
+
+        return numerator/denominator;
     }
 };
