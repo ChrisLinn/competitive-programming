a483c925d5c5ae7d5015c48417b6905bf4c2039e
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-22 17:17:41 +0800

    li-445] opt

--- LintCode/445.Cosine_Similarity/445.cpp
+++ LintCode/445.Cosine_Similarity/445.cpp
@@ -8,38 +8,36 @@ public:
     double cosineSimilarity(vector<int> &A, vector<int> &B) {
         // write your code here
 
-        double numerator, denominator1, denominator2, denominator;
+        double numerator, denominator_a, denominator_b;
         numerator = 0;
-        denominator1 = 0;
-        denominator2 = 0;
-        denominator = 0;
+        denominator_a = 0;
+        denominator_b = 0;
 
         if (A.size() != B.size()) {
             return 2.0000;
         }
 
+        if (!A.size()) {
+            return 2.0000;
+        }
+
+        // one loop
         for (std::vector<int>::iterator it_a = A.begin(), it_b = B.begin();
             it_a != A.end();
             ++it_a, ++it_b) {
             numerator += (*it_a)*(*it_b);
+            denominator_a += (*it_a)*(*it_a);
+            denominator_b += (*it_b)*(*it_b);
         }
 
-        for (auto a: A) {
-            denominator1 += a*a;
-        }
-        if (!denominator1) {
+        if (!denominator_a || !denominator_b) {
             return 2.0000;
         }
 
-        for (auto b: B) {
-            denominator2 += b*b;
-        }
-        if (!denominator2) {
-            return 2.0000;
-        }
+        // denominator_a = pow(denominator_a, 0.5);
+        denominator_a = sqrt(denominator_a);
+        denominator_b = sqrt(denominator_b);
 
-        denominator = pow(denominator1*denominator2, 0.5);
-
-        return numerator/denominator;
+        return numerator/denominator_a/denominator_b;
     }
 };
