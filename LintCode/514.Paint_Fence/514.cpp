db697ad4e869e661f289c4ff770ac4d1a0eb6dce
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-22 22:07:29 +0800

    li-514] update

---
+++ LintCode/514.Paint_Fence/514.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    /*
+     * @param n: non-negative integer, n posts
+     * @param k: non-negative integer, k colors
+     * @return: an integer, the total number of ways
+     */
+    int numWays(int n, int k) {
+        // write your code here
+        // JOSEPHUS??   register alloc?
+        int res;
+
+        if (n < k) {
+            res = 1
+            for (int i = 0; i < n; i ++) {
+                res *= 
+            }
+        }
+
+        return res;
+    }
+};
