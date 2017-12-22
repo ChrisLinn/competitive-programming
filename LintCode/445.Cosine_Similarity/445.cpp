9cd8f425b7678afe648f2f4848b0d289e6d93a8b
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-12-22 16:42:04 +0800

    li-445] init

---
+++ LintCode/445.Cosine_Similarity/445.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /*
+     * @param A: An integer array
+     * @param B: An integer array
+     * @return: Cosine similarity
+     */
+    double cosineSimilarity(vector<int> &A, vector<int> &B) {
+        // write your code here
+    }
+};
