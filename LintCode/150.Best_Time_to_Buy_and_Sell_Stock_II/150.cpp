eaa5ce4cd7012535a538171af38d6b417e435518
ChrisLinn <nanyishanshaoxiu@gmail.com>
2018-01-13 11:49:43 +0800

    li-150

--- LintCode/150.Best_Time_to_Buy_and_Sell_Stock_II/150.cpp
+++ LintCode/150.Best_Time_to_Buy_and_Sell_Stock_II/150.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
-    /*
+    /**
      * @param prices: Given an integer array
      * @return: Maximum profit
      */
     int maxProfit(vector<int> &prices) {
         // write your code here
+        int sz = prices.size();
+        int mx = 0;
+        for(int i=1;i<sz;i++){
+            if(prices[i]>prices[i-1]){
+                mx+=(prices[i]-prices[i-1]);
+            }
+        }
+        return mx;
     }
 };
