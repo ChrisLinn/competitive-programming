3136362615d50f08f5b5cb1700e1c85f58a38552
ChrisLinn <nanyishanshaoxiu@gmail.com>
2018-01-19 12:26:16 +1100

    add le-80 sol2

--- LeetCode/80-Remove_Duplicates_from_Sorted_Array_II/80.cpp
+++ LeetCode/80-Remove_Duplicates_from_Sorted_Array_II/80.cpp
@@ -17,3 +17,21 @@ public:
         return newLen;
     }
 };
+
+class Solution1 {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int newLen = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            //跳到一个新的，或下一个是新的
+            if (i > 0 && i < nums.size()-1 && nums[i] == nums[i-1] && nums[i] == nums[i+1]) {
+                continue;
+            }
+
+            nums[newLen++] = nums[i];
+        }
+
+        return newLen;
+    }
+};
