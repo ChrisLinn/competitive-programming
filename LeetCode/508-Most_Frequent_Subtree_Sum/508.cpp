0b4f6dcc920b5d021874c8fc58b0f6d529cd3058
ChrisLinn <nanyishanshaoxiu@gmail.com>
2018-02-20 07:39:11 +0800

    le-508] ac

--- LeetCode/508-Most_Frequent_Subtree_Sum/508.cpp
+++ LeetCode/508-Most_Frequent_Subtree_Sum/508.cpp
@@ -7,9 +7,63 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     vector<int> findFrequentTreeSum(TreeNode* root) {
+        vector<int> freqSum;
         
+        if(root) {
+            dfs(root);
+
+            sort(this->sums.begin(), this->sums.end());
+
+            int freq = 0;
+            int maxFreq = 0;
+            this->sums.push_back(this->sums.back() - 1);
+
+            for(vector<int>::iterator it = this->sums.begin()+1;
+                                    it != this->sums.end(); ++it) {
+                ++freq;
+                
+                if (*it != *(it-1)) {
+                    if(freq > maxFreq) {
+                        maxFreq = freq;
+                        freqSum.clear();
+                        freqSum.push_back(*(it-1));
+                    } else if(freq == maxFreq) {
+                        freqSum.push_back(*(it-1));                        
+                    }
+
+                    freq = 0;
+                }
+            }
+        }
+
+        return freqSum;
+    }
+
+private:
+    vector<int> sums;
+
+    int dfs(TreeNode* node){
+        int sum;
+
+        // if(!node) {
+        //    sum = 0;
+        // } else {
+            sum = node->val;
+
+            if(node->left) {
+                sum += dfs(node->left);
+            }
+            if(node->right) {
+                sum += dfs(node->right);
+            }
+
+            this->sums.push_back(sum);
+        // }
+
+        return sum;
     }
 };
