53d99187a1b07d8ce5933c74a631e491d0f9428f
ChrisLinn <nanyishanshaoxiu@gmail.com>
2018-03-03 13:46:35 +0800

    le-513] Line 41: member access within misaligned address 0x000000000021 for type 'struct TreeNode', which requires 8 byte alignment

--- LeetCode/513-Find_Bottom_Left_Tree_Value/513.cpp
+++ LeetCode/513-Find_Bottom_Left_Tree_Value/513.cpp
@@ -10,6 +10,49 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        
+        //Note: You may assume the tree (i.e., the given root node) is not NULL.
+
+        int max_d = getMaxDepth(root);
+
+        queue<TreeNode *> q;
+        q.push(root); 
+        int d = 1;
+        TreeNode *cur;
+        while(d<max_d) {
+            cur = q.front();
+            
+            if(cur->left) {
+                q.push(cur->left);
+            }
+            if(cur->right) {
+                q.push(cur->right);
+            }
+
+            q.pop();
+            d++;
+        }
+
+        while(1) {
+            cur = q.front();
+            if(cur->left) {
+                return cur->left->val;
+            }
+            if(cur->right) {
+                return cur->right->val;
+            }
+            q.pop();
+        }
+
+    }
+
+private:
+    int getMaxDepth(TreeNode* root) {
+        if(!root) {
+            return 0;
+        } else {
+            int l = getMaxDepth(root->left);
+            int r = getMaxDepth(root->right);
+            return 1 + ((l>=r)?l:r);
+        }
     }
 };
